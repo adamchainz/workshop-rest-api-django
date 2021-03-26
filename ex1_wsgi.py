59be2665016d28019e7ac88ff458ba50033b6355
Adam Johnson <me@adamj.eu>
2021-03-26 17:07:16 +0000

    Initial commit

---
+++ ex1_wsgi.py
@@ -0,0 +1,15 @@
+from wsgiref.simple_server import make_server
+
+
+def hello_world_app(environ, start_response):
+    status = "200 OK"
+    headers = [("Content-type", "text/html; charset=utf-8")]
+    body = [b"<h1>Hello World</h1>"]
+    start_response(status, headers)
+
+    return body
+
+
+with make_server("", 8000, hello_world_app) as httpd:
+    print("Serving on port 8000...")
+    httpd.serve_forever()
