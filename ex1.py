c0559aa786a69555edaa753886f885052a509c29
Adam Johnson <me@adamj.eu>
2021-03-27 12:06:28 +0000

    Skip WSGI examples

--- ex1.py
+++ ex1.py
@@ -8,11 +8,14 @@ from django.urls import path
 from django.utils.crypto import get_random_string
 from django.views.decorators.http import require_GET
 
+# Configure Django
+
 settings.configure(
+    # Use Django's debug mode when the environment asks for it:
     DEBUG=(os.environ.get("DEBUG", "") == "1"),
-    # Disable host header validation:
+    # Disable security feature - host header validation:
     ALLOWED_HOSTS=["*"],
-    # This module is the urlconf:
+    # Tell Django to read URL's from this module:
     ROOT_URLCONF=__name__,
     # We aren't using any security features but Django requires this setting:
     SECRET_KEY=get_random_string(50),
@@ -21,17 +24,25 @@ settings.configure(
 )
 
 
+# Our view function
+
+
 @require_GET
 def index(request):
     return HttpResponse("<h1>Hello World from Django!</h1>")
+
+
+# Our URL configuration
 
 
 urlpatterns = [
     path("", index),
 ]
 
+# Create a WSGI application so a web server could run this for us:
 app = get_wsgi_application()
 
+# Allow running Django's commands through this file:
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
